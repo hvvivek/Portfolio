becd964f59caa0300e216dc67996309030162c1f
Vivek HV <hvvivek2007@gmail.com>
2017-12-20 21:41:53 -0500

    Fixed Hover issue

--- js/script.js
+++ js/script.js
@@ -6,25 +6,6 @@ $(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
     });
-
-    // $('.img-card').on('mouseover', function () {
-    //     $('.vid-card').css("display", "block");    
-    // });
-
-    $('.img-card').on('mouseover', function () {
-        $('.vid-card').get(0).currentTime = 0    
-
-        if (!timeoutId) {
-            currentImage = this;
-            timeoutId = window.setTimeout(function() {
-                timeoutId = null; // EDIT: added this line
-                $(currentImage).css("display", "none");
-                $('.vid-card').css("display", "block");
-        }, 500);
-        }
-        
-    console.log("Basic Event Check");
-    });
     
     $('.vid-card').on('mouseout', function () {
         $(this).css("display", "none");
@@ -32,24 +13,26 @@ $(document).ready(function () {
     });
 
 
-    // $(".img-card").hover(function(e) {
-    //     if (!timeoutId) {
-    //         timeoutId = window.setTimeout(function() {
-    //             timeoutId = null; // EDIT: added this line
-    //             $(e).css("display", "none");
-    //             $('.vid-card').css("display", "block");  
-    //     }, 2000);
-    //     }
-    // },
-    // function () {
-    //     if (timeoutId) {
-    //         window.clearTimeout(timeoutId);
-    //         timeoutId = null;
-    //     }
-    //     else {
-    //     // $("#SeeAllEvents").slideUp('slow');
-    //     }
-    // });
+    $(".img-card").hover(function(e) {
+        $('.vid-card').get(0).currentTime = 2    
+        if (!timeoutId) {
+            currentImage = this;
+            timeoutId = window.setTimeout(function() {
+                timeoutId = null; // EDIT: added this line
+                $(currentImage).css("display", "none");
+                $('.vid-card').css("display", "block");  
+        }, 500);
+        }
+    },
+    function () {
+        if (timeoutId) {
+            window.clearTimeout(timeoutId);
+            timeoutId = null;
+        }
+        else {
+        // $("#SeeAllEvents").slideUp('slow');
+        }
+    });
 
 
 });
