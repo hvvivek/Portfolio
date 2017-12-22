e7f87b9f14a5ce130069ecb4d504bb683d159215
Vivek HV <hvvivek2007@gmail.com>
2017-12-21 21:31:38 -0500

    Updated Landing Page

--- js/script.js
+++ js/script.js
@@ -2,26 +2,30 @@ $(document).ready(function () {
 
     var timeoutId;
     var currentImage;
-
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
     });
     
     $('.vid-card').on('mouseout', function () {
         $(this).css("display", "none");
+        $(this)[0].pause();
+        $(this)[0].currentTime = 0;
         $('.img-card').css("display", "block");    
     });
 
 
     $(".img-card").hover(function(e) {
-        $('.vid-card').get(0).currentTime = 2    
         if (!timeoutId) {
             currentImage = this;
+            $( ".vid-card[toggle='" + $(currentImage).attr('target') + "']" )[0].play()  
+
             timeoutId = window.setTimeout(function() {
                 timeoutId = null; // EDIT: added this line
                 $(currentImage).css("display", "none");
-                $('.vid-card').css("display", "block");  
-        }, 500);
+                $( ".vid-card[toggle='" + $(currentImage).attr('target') + "']" ).css("display", "block");  
+                // console.log($(currentImage).attr('toggle'));
+                // $('.vid-card').css("display", "block");  
+        }, 700);
         }
     },
     function () {
@@ -34,5 +38,11 @@ $(document).ready(function () {
         }
     });
 
+        $(".content").on('mousewheel', function(event, delta) {
+            // console.log(event.originalEvent.deltaY)
+        this.scrollLeft += (event.originalEvent.deltaY);
+        event.preventDefault();
+        });
 
 });
+
